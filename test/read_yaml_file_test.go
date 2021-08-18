dfec74d7cb8e3fc921a5441b31159145d4615d9b
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2021-08-19 01:17:18 +0430

    unmarshal type checking test

--- test/read_yaml_file_test.go
+++ test/read_yaml_file_test.go
@@ -24,3 +24,38 @@ func TestParseLoadedYamlFile(t *testing.T) {
 		t.Error(errOnUnmarshal)
 	}
 }
+func TestParseLoadedYamlFileItems(t *testing.T) {
+	str, err := functions.ReadFileToString("sample.yaml")
+	if err != nil {
+		t.Error(err)
+	}
+	config := structs.Config{}
+	errOnUnmarshal := yaml.Unmarshal([]byte(str), &config)
+	if errOnUnmarshal != nil {
+		t.Error(errOnUnmarshal)
+	}
+	if config[0].Name!="api"{
+		t.Error("invalid parser for Name" )
+	}
+
+	if config[0].Become!=true{
+		t.Error("invalid parser for Become" )
+	}
+
+	if config[0].Exec[0].Name!="test"{
+		t.Error("invalid parser for Exec Name" )
+	}
+	if config[0].Exec[0].Command!="create"{
+		t.Error("invalid parser for Exec Command" )
+	}
+	if config[0].Exec[0].Type!="folder"{
+		t.Error("invalid parser for Exec Type" )
+	}
+	if config[0].Exec[0].Path!="/var/www/html/test"{
+		t.Error("invalid parser for Exec Path" )
+	}
+	if config[0].Exec[0].PassOnError!=true{
+		t.Error("invalid parser for Exec PassOnError" )
+	}
+
+}
