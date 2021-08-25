0008fb5509d64840e69655eba76673e7039325b2
arashrasoulzadeh <arashrasoulzadeh@gmail.com>
2021-08-25 17:35:01 +0430

    Ability to download deployment config from a url

--- functions/IO.go
+++ functions/IO.go
@@ -2,16 +2,56 @@ package functions
 
 import (
 	"arashrasoulzadeh/deepzy/logger"
+	"io"
 	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func ReadFileToString(path string) (string, error) {
 	logger.StepInfo("loading config from " + path)
+	if strings.HasPrefix(path, "http://") || strings.HasPrefix(path, "https://") {
+		logger.StepInfo("Downloading... " + path)
+		dlPath := "down_" + strconv.Itoa(rand.Int())
+		err := DownloadFile(dlPath, path)
+		if err != nil {
+			return "", err
+		}
+		path = dlPath
+		logger.StepInfo("Done Downloading " + path)
+
+	}
 	b, err := ioutil.ReadFile(path) // just pass the file name
 	if err != nil {
- 		return "nil", err
+		return "nil", err
 	}
 	str := string(b) // convert content to a 'string'
 
 	return str, err
+}
+
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
